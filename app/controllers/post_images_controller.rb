32cdc320be2918f02f825152eb2e95ac8584d842
minako-y <m.tp2uk@gmail.com>
2021-06-10 07:38:28 +0000

    [Finish]4-4ページング機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,5 +1,5 @@
 class PostImagesController < ApplicationController
-  
+
   def new
     @post_image = PostImage.new
   end
@@ -10,25 +10,25 @@ class PostImagesController < ApplicationController
     @post_image.save
     redirect_to post_images_path
   end
-  
+
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
     redirect_to post_images_path
   end
-  
-  
+
+
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
